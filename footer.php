ad3efb2786cd80681ec41e0c5393bc7af367a6a3
talum <tracidini@gmail.com>
2016-02-13 16:12:12 -0500

    Move footer widget

--- footer.php
+++ footer.php
@@ -5,8 +5,8 @@ footer
 ?>
 <footer>
   <p>Made and maintained by Tracy Lum</p>
-  <p>follow: github, medium, facebook, twitter, googleplus, instagram, linkedin, pinterest, goodreads</p>
+  <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer Widgets')); ?>
 </footer>
-<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer Widgets')); ?>
+
 <?php wp_footer(); ?>
 </html>
